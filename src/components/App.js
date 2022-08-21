a322b62e9b52dc4fbb604ac63648616d562428b9
Lisa Fink <lisadfink@gmail.com>
2022-08-21 15:43:07 -0400

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,25 @@ import { AuthProvider } from '../contexts/AuthContext';
 import '../styles/App.css';
 import Navigation from './Navigation';
 import SignUp from './SignUp';
+import Dashboard from './Dashboard';
+import Login from './LogIn';
+
+// Routes used to be Switch
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <Navigation />
-        <SignUp />
-      </AuthProvider>
+      <Router>
+        <AuthProvider>
+          <Navigation />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
