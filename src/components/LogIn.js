43427e9488e1ccb8d55a98ef646d79b1dac8d5e4
Lisa Fink <lisadfink@gmail.com>
2022-08-21 15:44:00 -0400

    Added login

---
+++ src/components/LogIn.js
@@ -0,0 +1,59 @@
+import React, { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+import '../styles/signup.css';
+
+const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      error && setError('');
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      navigate('/');
+    } catch {
+      // TODO show error reason
+      setError('Failed to sign in');
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="sign-up">
+      <h2>Log In</h2>
+      {error && <div className="error">{error}</div>}
+      <form onSubmit={handleSubmit} className="sign-up-form">
+        <div>
+          <label>Email</label>
+          <input type="email" required ref={emailRef}></input>
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input type="password" required ref={passwordRef}></input>
+        </div>
+
+        <button disabled={loading} type="submit">
+          Log In
+        </button>
+      </form>
+      <div>
+        Need an account? <Link to="/signup">Sign-up</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
