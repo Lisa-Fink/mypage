15fd32e41f8e357a1ef6f1ca20bf786fa0d1022d
Lisa Fink <lisadfink@gmail.com>
2022-08-21 16:28:58 -0400

    Added basic layout with log out

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+import '../styles/navigation.css';
 
 const Navigation = () => {
+  const [error, setError] = useState('');
+
+  const { currentUser, logout } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    setError('');
+    try {
+      await logout();
+      navigate('/login');
+    } catch {
+      setError('Failed to log out');
+    }
+  };
+
+  const userDiv = (
+    <>
+      <div>Profile</div>
+      <div>Friends</div>
+      <div> Friend Requests</div>
+      <div>{currentUser && currentUser.email}</div>
+      {error && <div>error</div>}
+
+      <button onClick={handleLogout}>Log Out</button>
+    </>
+  );
   return (
     <nav>
-      <div>MyPage</div>
-      <div>Login</div>
+      <div id="logo">
+        <h2>MyPage</h2>
+      </div>
+
+      <div id="user-div">{currentUser && userDiv}</div>
     </nav>
   );
 };
