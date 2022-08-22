184dd36446ccfb0e6436b9cf58932a2ecd5f9b4e
Lisa Fink <lisadfink@gmail.com>
2022-08-22 17:13:15 -0400

    Added user drop down menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import { doc, onSnapshot } from 'firebase/firestore';
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { db } from './firebaseConfig';
 
@@ -9,6 +9,8 @@ import '../styles/navigation.css';
 const Navigation = () => {
   const [error, setError] = useState('');
   const [photo, setPhoto] = useState('');
+  const [name, setName] = useState('');
+  const [showMenu, setShowMenu] = useState(false);
 
   const { currentUser, logout } = useAuth();
 
@@ -27,22 +29,44 @@ const Navigation = () => {
   currentUser &&
     onSnapshot(doc(db, 'users', currentUser.uid), (doc) => {
       setPhoto(doc.data().profilePic);
+      const first = doc.data().first;
+      const last = doc.data().last;
+      setName(`${first} ${last}`);
     });
+
+  const userMenu = (
+    <div id="user-menu">
+      <div>
+        <Link to="/update-account">Update Account</Link>
+      </div>
+      <div>
+        <button onClick={handleLogout}>Log Out</button>
+      </div>
+    </div>
+  );
 
   const userDiv = (
     <>
       <div>Profile</div>
       <div>Friends</div>
       <div> Friend Requests</div>
-      <div className="user-info">
-        {currentUser && currentUser.email}
-        {currentUser && (
-          <img className="profile-pic" src={photo} alt="profile" />
-        )}
+      <div
+        className="user-info"
+        onClick={() => {
+          setShowMenu(!showMenu);
+        }}
+        onMouseOver={() => setShowMenu(true)}
+        onMouseLeave={() => setShowMenu(false)}
+      >
+        <div id="user-info-nav">
+          {currentUser && (
+            <img className="profile-pic" src={photo} alt="profile" />
+          )}
+          {currentUser && name}
+        </div>
+        {showMenu && userMenu}
       </div>
       {error && <div>error</div>}
-
-      <button onClick={handleLogout}>Log Out</button>
     </>
   );
   return (
