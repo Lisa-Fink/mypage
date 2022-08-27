f89ed0757c477485cbee129b9ae840964c69dd3e
Lisa Fink <lisadfink@gmail.com>
2022-08-27 16:47:59 -0400

    Created footer with github link

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Created by Lisa Fink{' '}
+      <a href="https://github.com/Lisa-Fink/mypage">GitHub</a>
+    </div>
+  );
+};
+
+export default Footer;
